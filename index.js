53a8a65b01fa5c2b14bcd806abbb2f9d2d24d8cd
Nehal Udyavar <nehaludyavar@gmail.com>
2018-12-31 01:05:06 -0800

    Install passport + create Google auth service and route

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 
+require("./services/passport");
+
 app.get("/", (req, res) => {
   res.status(200).send({ message: "Welcome to Newt, the future of learning" });
 });
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT)
+require("./routes/authRoutes")(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
