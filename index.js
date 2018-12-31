8f2ac36661450ae7a41a9344e1adf79f624d36f5
Nehal Udyavar <nehaludyavar@gmail.com>
2018-12-31 02:08:48 -0800

    Install mongoose + setup mongo connection

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 // Require passport before initializing app
 require("./services/passport");
+
+// Connect to MongoDB
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
