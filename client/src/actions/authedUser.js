a8a370d436ba576089641976f1d3d6e06d05d833
Nehal Udyavar <nehaludyavar@gmail.com>
2019-01-01 00:24:25 -0800

    Install axios, redux-thunk + create fetchUser action creator

---
+++ client/src/actions/authedUser.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+export const FETCH_USER = 'FETCH_USER'
+
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user')
+
+  dispatch({
+    type: FETCH_USER,
+    payload: res
+  })
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 import './index.css'
 
 import App from './components/App'
 import reducers from './reducers'
+import middleware from './middleware'
 
-const store = createStore(reducers, {}, applyMiddleware())
+const store = createStore(reducers, {}, middleware)
 
 ReactDOM.render(
   <Provider store={ store }>
