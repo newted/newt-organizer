364407db74e286f61b09f0d13635fc81f00958fc
Nehal Udyavar <nehaludyavar@gmail.com>
2019-01-01 04:32:51 -0800

    Add Sidebar component

---
+++ client/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => (
+  <aside className="sidebar">
+    <div className="sidebar-navlinks">
+      <div>Sidebar</div>
+    </div>
+  </aside>
+)
+
+export default Sidebar
