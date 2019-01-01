0a7a7306ce34ea9cd0f07716d94dc0eda331114c
Nehal Udyavar <nehaludyavar@gmail.com>
2018-12-31 23:14:49 -0800

    Handle initial routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const Navbar = () => <h2>Navbar</h2>
+const Landing = () => <h2>Landing</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const Login = () => <h2>Login</h2>
 
 class App extends Component {
   render() {
     return (
-      <h1>Welcome to Newt.</h1>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Route exact path ='/' component={ Landing } />
+          <Route path='/dashboard' component={ Dashboard } />
+          <Route path='/login' component={ Login } />
+        </div>
+      </BrowserRouter>
     )
   }
 }
