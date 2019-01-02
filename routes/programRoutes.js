a92a74cc32900d1bc27024db050b8bca8052b6d5
Nehal Udyavar <nehaludyavar@gmail.com>
2019-01-01 23:16:34 -0800

    Add program route handler to create a new program

---
+++ routes/programRoutes.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const requireLogin = require("../middleware/requireLogin");
+
+const Program = mongoose.model("programs");
+
+module.exports = app => {
+  app.post("/api/programs", async (req, res) => {
+    // Get information from request body
+    const { name, shortname, provider } = req.body;
+
+    // Create Program
+    const program = new Program({
+      name,
+      shortname,
+      institution,
+      _user: req.user.id,
+      dateCreated: Date.now()
+    });
+
+    try {
+      // Add program to database
+      await survey.save();
+
+      // Send updated user information
+      res.status(201);
+    } catch (error) {
+      res.status(422).send(error);
+    }
+  });
+};
