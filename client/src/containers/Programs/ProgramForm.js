442d746c625b4645852cc68311de7d8be46a174d
Nehal Udyavar <nehaludyavar@gmail.com>
2019-01-04 00:55:26 -0800

    Create Program Field component for input + add inital field

--- client/src/containers/Programs/ProgramForm.js
+++ client/src/containers/Programs/ProgramForm.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
-import { reduxForm } from 'redux-form'
+import { reduxForm, Field } from 'redux-form'
+import ProgramField from './ProgramField'
 
 class ProgramForm extends Component {
+  renderFields() {
+    return (
+      <div>
+        <Field type='text' name='name' component={ ProgramField } />
+      </div>
+    )
+  }
+
   render() {
     return (
       <div>
-        Program Form
+        <form
+          onSubmit={ this.props.handleSubmit(values => console.log(values)) }
+        >
+          { this.renderFields() }
+          <button type='submit'>Submit</button>
+        </form>
       </div>
     )
   }
 }
 
 export default reduxForm({
-  form: 'ProgramForm'
+  form: 'programForm'
 })(ProgramForm)
