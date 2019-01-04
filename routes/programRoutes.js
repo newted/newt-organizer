c9b8ead53499846d340ae2827464858c2f1e62c4
Nehal Udyavar <nehaludyavar@gmail.com>
2019-01-04 13:56:55 -0800

    Create route handler to get user's programs

--- routes/programRoutes.js
+++ routes/programRoutes.js
@@ -4,6 +4,20 @@ const requireLogin = require("../middleware/requireLogin");
 const Program = mongoose.model("programs");
 
 module.exports = app => {
+  // GET request to receive a list of user's programs
+  app.get("/api/programs", requireLogin, async (req, res) => {
+    // Database query to get user's programs
+    const programs = await Program.find({
+      _user: req.user.id
+    }).select({
+      courses: false
+    });
+
+    // Send back user's programs
+    res.status(200).send(programs);
+  });
+
+  // POST request to create a program
   app.post("/api/programs", requireLogin, async (req, res) => {
     // Get information from request body
     const { name, shortname, institution } = req.body;
