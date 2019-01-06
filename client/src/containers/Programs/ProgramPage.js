b790aa53a214d50c7eb734a20843f6cf754eacfe
Nehal Udyavar <nehaludyavar@gmail.com>
2019-01-06 06:09:54 -0800

    Create page for each individual program

---
+++ client/src/containers/Programs/ProgramPage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+// Components
+import Navbar from '../../components/Navbar'
+import Sidebar from '../../components/Sidebar'
+// Styling
+import styles from './Dashboard.module.css'
+
+class ProgramPage extends Component {
+  render() {
+    // Redirect to Landing page if not authenticated
+    if(!this.props.auth) {
+      return <Redirect to='/' />
+    }
+
+    return (
+      <div className={ styles.appContainer }>
+        <Sidebar />
+        <section className={ styles.pageContainer }>
+          <Navbar />
+          <div className={ styles.mainContainer }>
+            <div className={ styles.contentContainer }>
+              <h2 className={ styles.header }>ProgramPage</h2>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(ProgramPage)
