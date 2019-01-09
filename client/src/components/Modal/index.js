cbb727ebfc0e6e7ed34be215b2b50e86a92ed4b9
Nehal Udyavar <nehaludyavar@gmail.com>
2019-01-09 13:34:17 -0800

    Create Modal Body and Modal Footer sub-components

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -4,6 +4,8 @@ import styles from './Modal.module.css'
 import findByComponentType from '../../utils/findByComponentType'
 
 const Header = () => null
+const Body = () => null
+const Footer = () => null
 
 class Modal extends Component {
   showHideClassName() {
@@ -16,7 +18,6 @@ class Modal extends Component {
 
   renderModalHeader() {
     const { children } = this.props
-
     const header = findByComponentType(children, Header)
 
     if (!header) {
@@ -28,14 +29,46 @@ class Modal extends Component {
     )
   }
 
+  renderModalBody() {
+    const { children } = this.props
+    const body = findByComponentType(children, Body)
+
+    if (!body) {
+      return null
+    }
+
+    return (
+      <div> { body.props.children }</div>
+    )
+  }
+
+  renderModalFooter() {
+    const { children } = this.props
+    const footer = findByComponentType(children, Footer)
+
+    if (!footer) {
+      return null
+    }
+
+    return (
+      <div>{ footer.props.children }</div>
+    )
+  }
+
   render() {
-    const { handleClose, showModal } = this.props
+    const { handleClose } = this.props
 
     return (
       <div className={ this.showHideClassName() }>
         <div className={ styles.modalMain }>
           <div>
             { this.renderModalHeader() }
+          </div>
+          <div>
+            { this.renderModalBody() }
+          </div>
+          <div>
+            { this.renderModalFooter() }
           </div>
           <Button
             text='Close'
@@ -48,5 +81,7 @@ class Modal extends Component {
 }
 
 Modal.Header = Header
+Modal.Body = Body
+Modal.Footer = Footer
 
 export default Modal
