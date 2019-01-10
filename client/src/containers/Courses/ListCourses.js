c98f8c375ac40f6d65177c65a13848738584e7ab
Nehal Udyavar <nehaludyavar@gmail.com>
2019-01-10 11:08:27 -0800

    Add Course button on List courses container

--- client/src/containers/Courses/ListCourses.js
+++ client/src/containers/Courses/ListCourses.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import Button from '../../components/Button'
 import styles from './ListCourses.module.css'
 
 class ListCourses extends Component {
   render() {
     return (
       <div className={ styles.courseContainer }>
-        <h3>Courses</h3>
+        <div className={ styles.headerContainer }>
+          <h3>Courses</h3>
+          <Button
+            text='Add Course'
+            additionalClass={ styles.addBtn }
+          />
+        </div>
       </div>
     )
   }
