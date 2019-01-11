0918ee8d2a861a24ca62395869f08538df6e47f2
Nehal Udyavar <nehaludyavar@gmail.com>
2019-01-10 23:12:57 -0800

    Create shareable and customizable Card component

---
+++ client/src/components/Card/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+// Styling
+import styles from './Card.module.css'
+
+export default ({ path, icon, title, subtitle, additionalClass }) => (
+  <Link
+    to={{ pathname: path }}
+    className={ `${styles.card} ${styles.additionalClass}` }
+  >
+    <div className={ styles.cardBody }>
+      { icon && (
+        <div className={ styles.icon }>
+          { icon }
+        </div>
+      )}
+      { title && (
+        <div className={ styles.title }>
+          { title }
+        </div>
+      )}
+      { subtitle && (
+        <div className={ styles.subtitle }>
+          { subtitle }
+        </div>
+      )}
+    </div>
+  </Link>
+)
