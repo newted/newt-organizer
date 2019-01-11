0c6f0cdccaec23e2c64ab96ee00519e1d7014ab3
Nehal Udyavar <nehaludyavar@gmail.com>
2019-01-11 09:48:28 -0800

    Render course cards

--- client/src/containers/Courses/ListCourses.js
+++ client/src/containers/Courses/ListCourses.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import _ from 'lodash'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+// Components
 import Button from '../../components/Button'
+import Card from '../../components/Card'
+// Styling
 import styles from './ListCourses.module.css'
+import { BookIcon } from '../../utils/icons'
 
 class ListCourses extends Component {
+  renderCards() {
+    const { programId, courses } = this.props
+    console.log(courses)
+
+    return _.map(courses, ({ _id, name }) => {
+      return (
+        <Card
+          path={ `/programs/${programId}/courses/${_id}` }
+          title={ name }
+          icon={ BookIcon }
+          additionalClass={ styles.cardColor }
+          key={ _id }
+        />
+      )
+    })
+  }
+
   render() {
     const { programId } = this.props
 
@@ -19,14 +41,26 @@ class ListCourses extends Component {
             />
           </Link>
         </div>
+        <div className={ styles.cardContainer }>
+          { this.renderCards() }
+        </div>
       </div>
     )
   }
 }
 
-function mapStateToProps(_, { programId }) {
+function mapStateToProps({ programs }, { programId }) {
+  const program = _.filter(
+    programs.items,
+    program => program._id === programId
+  )[0]
+
+  const courses = program.courses
+  console.log(courses)
+
   return {
-    programId
+    programId,
+    courses
   }
 }
 
--- client/src/utils/icons.js
+++ client/src/utils/icons.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { FaUniversity } from 'react-icons/fa'
+import { FiBook } from 'react-icons/fi'
 
 const iconSize = 30
 
 export const UniversityIcon = <FaUniversity size={ iconSize } />
+export const BookIcon = <FiBook size={ iconSize } />
