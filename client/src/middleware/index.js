b9a425a710e46037e94ae4c1372e6156b2c0df26
Nehal Udyavar <nehaludyavar@gmail.com>
2019-02-16 18:06:06 -0800

    Don't apply logger middleware in production

--- client/src/middleware/index.js
+++ client/src/middleware/index.js
@@ -2,7 +2,17 @@ import thunk from 'redux-thunk'
 import logger from './logger'
 import { applyMiddleware } from 'redux'
 
-export default applyMiddleware(
-  thunk,
-  logger
-)
+let middleware
+
+// Not the way the docs recommends to do it (because I've loaded middleware I
+// may not require), but that way kept throwing an error "Middleware is not a
+// function."
+// See: https://redux.js.org/api/applymiddleware
+// and: https://github.com/reduxjs/redux/issues/581#issuecomment-133187076
+if (process.env.NODE_ENV !== 'production') {
+  middleware = applyMiddleware(thunk, logger)
+} else {
+  middleware = applyMiddleware(thunk)
+}
+
+export default middleware
