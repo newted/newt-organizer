7222ccc208d841a2cbc5e9f38f410bc50f9d1dcd
Nehal Udyavar <nehaludyavar@gmail.com>
2019-02-17 17:32:14 -0800

    Add production config variables for firebase client and admin

---
+++ client/src/config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  firebaseApiKey: "AIzaSyBCxBXY5OxxtZCxZL4GKk9LDivtYfcXtog",
+  firebaseAuthDomain: "newt-organizer-prod.firebaseapp.com",
+  firebaseDatabaseUrl: "https://newt-organizer-prod.firebaseio.com",
+  firebaseProjectId: "newt-organizer-prod",
+  firebaseStorageBucket: "newt-organizer-prod.appspot.com",
+  firebaseMessagingSenderId: "732281447687"
+}
--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,15 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  firebaseType: process.env.FIREBASE_TYPE,
+  firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+  firebasePrivateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+  firebasePrivateKey: process.env.FIREBASE_PRIVATE_KEY,
+  firebaseClientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  firebaseClientId: process.env.FIREBASE_CLIENT_ID,
+  firebaseAuthUri: process.env.FIREBASE_AUTH_URI,
+  firebaseTokenUri: process.env.FIREBASE_TOKEN_URI,
+  firebaseAuthProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  firebaseClientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL
 };
