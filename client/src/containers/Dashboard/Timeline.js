def0d415d0bcaeee2942e789c1a68c36b141e481
Nehal Udyavar <nehaludyavar@gmail.com>
2019-02-22 15:47:08 -0800

    Programmatically render Timeline Cards

--- client/src/containers/Dashboard/Timeline.js
+++ client/src/containers/Dashboard/Timeline.js
@@ -1,33 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
+import _ from 'lodash'
+import { connect } from 'react-redux'
 // Components
 import TimelineCard from './TimelineCard'
 // Styling
 import styles from './Timeline.module.css'
 
-const Timeline = () => (
-  <div className={ styles.container }>
-    <div className={ styles.agendaContainer }>
-      <h3 className={ styles.header }>Your Upcoming Week</h3>
-      <div className={ styles.timeline }>
-        <h4 className={ styles.date }>22 Feb 2019</h4>
-        <TimelineCard
-          title='JSX Assignment 1'
-          course='React Nanodegree'
-          details='Learn about JSX, a JavaScript syntax extension.'
-        />
-        <TimelineCard
-          title='The Mongols'
-          course='Crash Course History'
-          details='Learn about JSX, a JavaScript syntax extension.'
-        />
-      </div>
-    </div>
-    <div className={ styles.prevWeekContainer }>
-      <h3 className={ styles.header }>Previous Week</h3>
-      <div className={ styles.prevWeek }>
-      </div>
-    </div>
-  </div>
-)
+class Timeline extends Component {
+  renderCards() {
+    const { assignments } = this.props
 
-export default Timeline
+    return _.map(assignments, ({ _id, name, courseName, details }) => (
+      <TimelineCard
+        title={ name }
+        course={ courseName }
+        details={ details }
+        key={ _id }
+      />
+    ))
+  }
+  render() {
+    return (
+      <div className={ styles.container }>
+        <div className={ styles.agendaContainer }>
+          <h3 className={ styles.header }>Your Upcoming Week</h3>
+          <div className={ styles.timeline }>
+            <h4 className={ styles.date }>22 Feb 2019</h4>
+            { this.renderCards() }
+          </div>
+        </div>
+        <div className={ styles.prevWeekContainer }>
+          <h3 className={ styles.header }>Previous Week</h3>
+          <div className={ styles.prevWeek }>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({ courses, programs }) {
+  const assignments = []
+
+  _.forEach(courses.items, course => {
+    _.forEach(course.assignments, assignment => {
+      assignment['courseName'] = courses.items[course._id].name
+      assignments.push(assignment)
+    })
+  })
+
+  return {
+    assignments
+  }
+}
+
+export default connect(mapStateToProps)(Timeline)
