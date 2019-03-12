d973728d491b5d27d3473e6e5ab79b2abfbfe930
Nehal Udyavar <nehaludyavar@gmail.com>
2019-03-12 22:05:15 +0400

    Make demo assignments due dates based on current date

--- client/src/utils/demoData.js
+++ client/src/utils/demoData.js
@@ -1,3 +1,16 @@
+import moment from 'moment'
+
+// Create a due date based on the number of days prior to today, so that the
+// demo data is always relevant and populates the UI.
+const prevDueDater = (numDays) => {
+  return moment().subtract(numDays, 'days').format()
+}
+
+// Create future due date based on number of days from today.
+const futureDueDater = (numDays) => {
+  return moment().add(numDays, 'days').format()
+}
+
 const programs = {
   P001: {
     courses: ["C001", "C002"],
@@ -8,10 +21,10 @@ const programs = {
     _user: "demouser"
   },
   P002: {
-    courses: ["C003", "C004"],
+    courses: ["C003"],
     dateCreated: "2019-02-19T21:25:23.916Z",
-    institution: "British Columbia Institute of Technology",
-    name: "Business Intelligence I",
+    institution: "Crash Course",
+    name: "Crash Course History",
     _id: "P002",
     _user: "demouser"
   }
@@ -23,7 +36,7 @@ const courses = {
       {
         completed: true,
         dateCreated: "2019-02-19T21:25:48.205Z",
-        dateDue: "2019-02-21T08:00:00.000Z",
+        dateDue: prevDueDater(20),
         details: "Learn about JSX, a JavaScript syntax extension.",
         inProgress: false,
         name: "Practicing JSX",
@@ -32,7 +45,7 @@ const courses = {
       {
         completed: false,
         dateCreated: "2019-02-20T21:25:48.205Z",
-        dateDue: "2019-02-26T08:00:00.000Z",
+        dateDue: prevDueDater(10),
         details: "The building blocks of React.",
         inProgress: true,
         name: "Function and Class Components",
@@ -41,11 +54,20 @@ const courses = {
       {
         completed: false,
         dateCreated: "2019-02-20T23:25:48.205Z",
-        dateDue: "2019-02-27T08:00:00.000Z",
+        dateDue: futureDueDater(1),
         details: "Storing and using component-specific data.",
         inProgress: false,
         name: "Managing State",
         _id: "A003"
+      },
+      {
+        completed: false,
+        dateCreated: "2019-02-20T23:25:48.205Z",
+        dateDue: futureDueDater(8),
+        details: "Handling events with React elements.",
+        inProgress: false,
+        name: "Handling Events",
+        _id: "A010"
       }
     ],
     dataCreated: "2019-02-19T21:25:23.916Z",
@@ -56,9 +78,9 @@ const courses = {
   C002: {
     assignments: [
       {
-        completed: false,
+        completed: true,
         dateCreated: "2019-02-20T21:25:48.205Z",
-        dateDue: "2019-03-08T08:00:00.000Z",
+        dateDue: prevDueDater(11),
         inProgress: false,
         name: "Actions and Reducers",
         _id: "A004"
@@ -66,7 +88,7 @@ const courses = {
       {
         completed: false,
         dateCreated: "2019-02-20T21:25:55.205Z",
-        dateDue: "2019-03-08T08:00:00.000Z",
+        dateDue: prevDueDater(4),
         inProgress: false,
         name: "Redux Store",
         _id: "A005"
@@ -78,18 +100,48 @@ const courses = {
     _id: "C002"
   },
   C003: {
-    assignments: [],
+    assignments: [
+      {
+        completed: false,
+        dateCreated: "2019-02-20T21:25:48.205Z",
+        dateDue: prevDueDater(3),
+        details: "John Green investigates the dawn of human civilization.",
+        inProgress: false,
+        name: "The Agricultural Revolution",
+        _id: "A006"
+      },
+      {
+        completed: true,
+        dateCreated: "2019-02-20T21:25:48.205Z",
+        dateDue: prevDueDater(1),
+        details: "John Green teaches you about one of the largest of the ancient civilizations.",
+        inProgress: false,
+        name: "Indus Valley Civilization",
+        _id: "A007"
+      },
+      {
+        completed: false,
+        dateCreated: "2019-02-20T21:25:48.205Z",
+        dateDue: futureDueDater(1),
+        details: "John presents Mesopotamia, and the early civilizations that arose around the Fertile Crescent",
+        inProgress: false,
+        name: "Mesopotamia",
+        _id: "A008"
+      },
+      {
+        completed: false,
+        dateCreated: "2019-02-20T21:25:48.205Z",
+        dateDue: futureDueDater(4),
+        details: "John covers the long, long history of ancient Egypt.",
+        inProgress: false,
+        name: "Ancient Egypt",
+        _id: "A009"
+      },
+    ],
     dateCreated: "2019-02-20T21:25:23.916Z",
-    name: "Fundamentals of Data Visualization",
+    name: "Crash Course World History",
     programId: "P002",
     _id: "C003"
-  },
-  C004: {
-    assignments: [],
-    dateCreated: "2019-02-20T21:35:23.916Z",
-    name: "Intro to Tableau",
-    programId: "P002",
-    _id: "C004"
   }
 };
 
