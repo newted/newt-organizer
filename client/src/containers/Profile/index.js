e03acdeb6f1cc0de0dc0754b79b32d658385f572
Nehal Udyavar <nehaludyavar@gmail.com>
2019-03-15 13:09:35 +0400

    Basic Profile UI + options tab switching functionality

--- client/src/containers/Profile/index.js
+++ client/src/containers/Profile/index.js
@@ -1,16 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 // Styling
-import styles from './Profile.module.css'
+import styles from "./Profile.module.css";
 
 class Profile extends Component {
+  state = {
+    activeTab: "Personal Information"
+  };
+
+  renderOptionClass(optionTitle) {
+    return this.state.activeTab === optionTitle
+      ? `${styles.option} ${styles.active}`
+      : styles.option;
+  }
+
+  handleTabChange = e => {
+    const tab = e.target.innerHTML;
+
+    this.setState(() => ({
+      activeTab: tab
+    }));
+  };
+
   render() {
     return (
       <div className={styles.mainContainer}>
-        <h2 className={styles.header}>Profile</h2>
-        <div className={styles.container}></div>
+        <h2 className={styles.headerContainer}>Profile</h2>
+        <div className={styles.container}>
+          <div className={styles.optionsContainer}>
+            <h3 className={styles.subheading}>Account</h3>
+            <div
+              className={this.renderOptionClass("Personal Information")}
+              onClick={e => this.handleTabChange(e)}
+            >
+              Personal Information
+            </div>
+            <div
+              className={this.renderOptionClass("Statistics")}
+              onClick={e => this.handleTabChange(e)}
+            >
+              Statistics
+            </div>
+          </div>
+          <div className={styles.contentContainer} />
+        </div>
       </div>
     );
   }
 }
 
-export default Profile
+export default Profile;
