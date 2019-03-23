77e4adb2a3fee1ed9598ef97599c56336daa5c52
Nehal Udyavar <nehaludyavar@gmail.com>
2019-03-22 19:30:56 -0700

    Replace redundant switch statements in Navbar + comments

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -60,63 +60,64 @@ class Navbar extends Component {
 
   // Render Login or Logout based on authentication state
   renderButtons() {
-    switch (this.props.auth.exists) {
-      case false:
-        return (
-          <Link to="/login">
-            <Button additionalClass={styles.loginBtn}>Sign in</Button>
-          </Link>
-        );
-      default:
-        return (
-          <div className={styles.navbarBtns}>
-            {/* If Landing page and logged in, show 'To Dashboard' link */}
-            {this.props.theme === "landing" ? (
-              <Fragment>
-                <Link to="/dashboard">
-                  <div className={styles.toDash}>Go to Dashboard</div>
-                </Link>
-                <Button
-                  additionalClass={styles.loginBtn}
-                  onClick={this.props.signOut}
-                >
-                  Log out
-                </Button>
-              </Fragment>
-            ) : (
-              <div className={styles.userIcon}>
-                <Dropdown
-                  visible={this.state.showDropdown}
-                  handleOpen={event => this.openDropdown(event)}
-                  style={{ height: "22px" }}
-                >
-                  <FiUser size={22} />
-                  <Dropdown.Menu>
-                    <Dropdown.Item
-                      onClick={() => this.props.history.push("/profile")}
-                    >
-                      Profile
-                    </Dropdown.Item>
-                    <Dropdown.Item onClick={this.props.signOut}>
-                      Logout
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-            )}
-          </div>
-        );
+    // If not authenticated, return Sign In button on Navbar
+    if (!this.props.auth.exists) {
+      return (
+        <Link to="/login">
+          <Button additionalClass={styles.loginBtn}>Sign in</Button>
+        </Link>
+      );
     }
+
+    // If authenticated, return Go to Dashboard and Logout button if on landing
+    // page, otherwise the Profile icon with a dropdown.
+    return (
+      <div className={styles.navbarBtns}>
+        {/* If Landing page and logged in, show 'To Dashboard' link */}
+        {this.props.theme === "landing" ? (
+          <Fragment>
+            <Link to="/dashboard">
+              <div className={styles.toDash}>Go to Dashboard</div>
+            </Link>
+            <Button
+              additionalClass={styles.loginBtn}
+              onClick={this.props.signOut}
+            >
+              Log out
+            </Button>
+          </Fragment>
+        ) : (
+          <div className={styles.userIcon}>
+            <Dropdown
+              visible={this.state.showDropdown}
+              handleOpen={event => this.openDropdown(event)}
+              style={{ height: "22px" }}
+            >
+              <FiUser size={22} />
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  onClick={() => this.props.history.push("/profile")}
+                >
+                  Profile
+                </Dropdown.Item>
+                <Dropdown.Item onClick={this.props.signOut}>
+                  Logout
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        )}
+      </div>
+    );
   }
 
   // Render navbar background color based on whether it's landing page or not
   renderNavbarColor() {
-    switch (this.props.theme) {
-      case "landing":
-        return styles.lightBlueNavbar;
-      default:
-        return styles.lightGreyNavbar;
+    if (this.props.theme === "landing") {
+      return styles.lightBlueNavbar;
     }
+
+    return styles.lightGreyNavbar;
   }
 
   render() {
