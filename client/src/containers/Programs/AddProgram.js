242e9dc468e8ae80af94c44f9965d8b0349775aa
Nehal Udyavar <nehaludyavar@gmail.com>
2019-04-04 00:08:08 -0700

    Delete Add Program Form and switch to general Form component

--- client/src/containers/Programs/AddProgram.js
+++ client/src/containers/Programs/AddProgram.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import programFields from "./programFields";
 // Components
-import AddProgramForm from "./AddProgramForm";
+import Form from "../../components/Form";
 // API
 import { createProgram } from "../../actions/programs";
 // Styling
@@ -23,7 +24,13 @@ class AddProgram extends Component {
         <div className={styles.headerContainer}>
           <h3>Add a New Program</h3>
         </div>
-        <AddProgramForm onSubmit={this.props.createProgram} />
+        <Form
+          formName="AddProgramForm"
+          formFields={programFields}
+          onSubmit={values =>
+            this.props.createProgram(values, this.props.history)
+          }
+        />
       </div>
     );
   }
@@ -31,7 +38,7 @@ class AddProgram extends Component {
 
 const mapDispatchToProps = {
   createProgram
-}
+};
 
 export default connect(
   null,
