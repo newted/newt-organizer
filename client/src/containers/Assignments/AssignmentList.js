3fbf1f8d54c244dcefe018ff31953ea79a496e7c
Nehal Udyavar <nehaludyavar@gmail.com>
2019-05-12 20:49:10 -0700

    Remove Assignment Table and begin redesign wireframe

--- client/src/containers/Assignments/AssignmentList.js
+++ client/src/containers/Assignments/AssignmentList.js
@@ -1,25 +1,16 @@
 import React, { Component } from "react";
 import _ from "lodash";
 import { connect } from "react-redux";
-import { allAssignmentTableFields } from "./assignmentFields";
 import { statusDueDateSort } from "../../utils/containerHelpers";
-// Components
-import Button from "../../components/Button";
-import AssignmentTable from "./AssignmentTable";
 // Styling
 import styles from "./AssignmentList.module.css";
 
 class AssignmentList extends Component {
   state = {
-    showDropdown: {}, // Object for multiple assignment dropdowns
     showCompleted: false, // Doesn't show completed assignments by default
-    currentDropdownId: null // Passed to closeDropdown function so it knows which table row dropdown to close.
   };
 
-  _isMounted = false;
-
   componentDidMount() {
-    this._isMounted = true;
     // If all the assignments are complete, this sets the value of
     // showCompleted to true, otherwise it's kept as false. This way if all the
     // assignments are complete, the initial UI state is to show the completed
@@ -32,58 +23,6 @@ class AssignmentList extends Component {
     }));
   }
 
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  openDropdown = (assignmentId, event) => {
-    if (this._isMounted) {
-      this.setState(
-        prevState =>
-          // If currentDropdownId exists (is not null), then close that dropdown
-          // so that multiple aren't open.
-          prevState.currentDropdownId
-            ? {
-                showDropdown: {
-                  ...prevState.showDropdown,
-                  [prevState.currentDropdownId]: false, // Close the previous dropdown so multiple aren't open
-                  [assignmentId]: true
-                },
-                currentDropdownId: assignmentId
-              }
-            : {
-                showDropdown: {
-                  ...prevState.showDropdown,
-                  [assignmentId]: true
-                },
-                currentDropdownId: assignmentId
-              },
-        () => {
-          document.addEventListener("click", this.closeDropdown);
-        }
-      );
-    }
-  };
-
-  closeDropdown = () => {
-    const assignmentId = this.state.currentDropdownId;
-
-    if (this._isMounted) {
-      this.setState(
-        prevState => ({
-          showDropdown: {
-            ...prevState.showDropdown,
-            [assignmentId]: false
-          },
-          currentDropdownId: null
-        }),
-        () => {
-          document.removeEventListener("click", this.closeDropdown);
-        }
-      );
-    }
-  };
-
   handleShowCompleted = e => {
     e.preventDefault();
 
@@ -92,36 +31,20 @@ class AssignmentList extends Component {
     }));
   };
 
-  render() {
-    const { assignments, numCompleted } = this.props;
-    const { showCompleted } = this.state;
+  renderAssignmentList() {
+    const { assignments } = this.props
+    return _.map(assignments, ({ name, _id }) => (
+      <h4 key={_id}>{name}</h4>
+    ))
+  }
 
+  render() {
     return (
       <div className={styles.mainContainer}>
-        <div className={styles.headerContainer}>
-          <h2>Your Assignments</h2>
-        </div>
-        <div className={styles.tableContainer}>
-          <div className={styles.tableOptions}>
-            <Button
-              additionalClass={
-                showCompleted
-                  ? [styles.completedBtn, styles.selected].join(" ")
-                  : styles.completedBtn
-              }
-              onClick={this.handleShowCompleted}
-            >
-              {`Show Completed (${numCompleted})`}
-            </Button>
-          </div>
-          <AssignmentTable
-            fields={allAssignmentTableFields}
-            assignments={assignments}
-            name="Assignments"
-            showCompleted={this.state.showCompleted}
-            dropdownVisible={this.state.showDropdown}
-            handleOpenDropdown={this.openDropdown}
-          />
+        <h2 className={styles.headerContainer}>Your Assignments</h2>
+        <div className={styles.container}>
+          <div className={styles.listContainer}>{ this.renderAssignmentList() }</div>
+          <div className={styles.contentContainer}></div>
         </div>
       </div>
     );
