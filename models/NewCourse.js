efa297f0c313da50f9f40dc6c78f2ff89d01e8c2
Nehal Udyavar <nehaludyavar@gmail.com>
2019-09-06 11:44:52 -0700

    Create NewCourse model to tie in with Collection and Content

---
+++ models/NewCourse.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const newCourseSchema = new Schema({
+  name: String,
+  shortname: String,
+  creator: {
+    type: String,
+    default: "user"
+  },
+  collections: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Collection"
+    }
+  ],
+  individualContent: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "UserContent"
+    }
+  ],
+  _user: String,
+  dateCreated: Date,
+  lastUpdated: Date
+});
+
+mongoose.model("new-courses", newCourseSchema);
