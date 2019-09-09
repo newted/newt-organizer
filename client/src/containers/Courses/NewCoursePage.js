5a3aea6146e2c6b724dc5aded48b5673c1162dd6
Nehal Udyavar <nehaludyavar@gmail.com>
2019-09-09 12:38:53 -0700

    Course update form on Modal UI

--- client/src/containers/Courses/NewCoursePage.js
+++ client/src/containers/Courses/NewCoursePage.js
@@ -7,9 +7,16 @@ import {
   HeaderContainer
 } from "../../components/PageContainers";
 import Loader from "../../components/Loader";
+import Button from "../../components/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { Formik } from "formik";
+// Styles
+import styles from "./NewCourseList.module.css";
 
 const NewCoursePage = ({ match }) => {
   const [currentCourse, setCurrentCourse] = useState({});
+  const [showModal, setShowModal] = useState(false);
 
   // Get courseId from URL
   const { courseId } = match.params;
@@ -28,11 +35,76 @@ const NewCoursePage = ({ match }) => {
     return <Loader />;
   }
 
+  // Functions to set modal show state to true and false
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+
   return (
     <MainContainer>
       <HeaderContainer>
         <h2>{currentCourse.name}</h2>
+        <Button
+          category="primary"
+          onClick={handleShowModal}
+          style={{ width: "100px" }}
+        >
+          Edit
+        </Button>
       </HeaderContainer>
+      <Modal show={showModal} onHide={handleCloseModal} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Edit {currentCourse.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className={styles.modalBody}>
+          <Formik
+            initialValues={{
+              name: currentCourse.name,
+              shortname: currentCourse.shortname
+            }}
+            onSubmit={values => console.log(values)}
+          >
+            {({
+              handleSubmit,
+              handleChange,
+              handleBlur,
+              values,
+              touched,
+              isValid,
+              errors
+            }) => (
+              <Form noValidate onSubmit={handleSubmit} className={styles.form}>
+                <Form.Group controlId="courseName">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <Form.Group controlId="courseShortName">
+                  <Form.Label>Short Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="shortname"
+                    value={values.shortname}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <div className={styles.buttonContainer}>
+                  <Button
+                    category="primary"
+                    type="submit"
+                    style={{ width: "200px", marginTop: "1rem" }}
+                  >
+                    Update
+                  </Button>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </Modal.Body>
+      </Modal>
     </MainContainer>
   );
 };
