127518bb4dd334010d8e403d4e28f48f11b9d966
Nehal Udyavar <nehaludyavar@gmail.com>
2019-09-09 02:20:57 -0700

    Render course cards instead of stringified json

--- client/src/containers/Courses/NewCoursePage.js
+++ client/src/containers/Courses/NewCoursePage.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import _ from "lodash";
 // Components
 import Button from "../../components/Button";
+import Card from "../../components/Card";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import { Formik } from "formik";
@@ -9,20 +11,39 @@ import { Formik } from "formik";
 import { CREATE_NEW_COURSE, createCourse } from "../../actions/newCourses";
 // Styling
 import styles from "./NewCoursePage.module.css";
+import { UniversityIcon } from "../../utils/icons";
 
 const NewCoursePage = () => {
   const [showModal, setShowModal] = useState(false);
 
+  // Functions to set modal show state to true and false
   const handleShowModal = () => setShowModal(true);
   const handleCloseModal = () => setShowModal(false);
 
+  // Get courses data from global state
   const courses = useSelector(state => state.newCourses);
+  // Get dispatch
   const dispatch = useDispatch();
 
+  // Function to handle form submission (API request + dispatch action)
   const handleFormSubmit = async values => {
     const data = await createCourse(values);
     dispatch({ type: CREATE_NEW_COURSE, payload: data });
     handleCloseModal();
+  };
+
+  // Function to render course cards
+  const renderCards = () => {
+    if (!_.isEmpty(courses.items)) {
+      return _.map(courses.items, course => (
+        <Card
+          title={course.name}
+          icon={UniversityIcon}
+          additionalClass={styles.cardColor}
+          key={course._id}
+        />
+      ));
+    }
   };
 
   return (
@@ -33,6 +54,7 @@ const NewCoursePage = () => {
           Create Course
         </Button>
       </div>
+      <div className={styles.cardContainer}>{renderCards()}</div>
       <Modal show={showModal} onHide={handleCloseModal} size="lg">
         <Modal.Header closeButton>
           <Modal.Title>Create a Course</Modal.Title>
@@ -84,7 +106,6 @@ const NewCoursePage = () => {
           </Formik>
         </Modal.Body>
       </Modal>
-      {JSON.stringify(courses.items)}
     </div>
   );
 };
