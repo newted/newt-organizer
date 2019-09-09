2e66e57b864c12882baeaf61690aa7e4a44ac85b
Nehal Udyavar <nehaludyavar@gmail.com>
2019-09-09 11:47:56 -0700

    Get course info and display in Course Page

--- client/src/containers/Courses/NewCoursePage.js
+++ client/src/containers/Courses/NewCoursePage.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import _ from "lodash";
 // Components
-import { MainContainer } from "../../components/PageContainers";
+import {
+  MainContainer,
+  HeaderContainer
+} from "../../components/PageContainers";
+import Loader from "../../components/Loader";
 
-const NewCoursePage = props => (
-  <MainContainer>{JSON.stringify(props)}</MainContainer>
-);
+const NewCoursePage = ({ match }) => {
+  const [currentCourse, setCurrentCourse] = useState({});
+
+  // Get courseId from URL
+  const { courseId } = match.params;
+  // Get courses data from global state
+  const courses = useSelector(state => state.newCourses);
+
+  useEffect(() => {
+    // Filter out course which matches page's courseId
+    const [course] = _.filter(courses.items, course => course._id === courseId);
+    // Set state
+    setCurrentCourse(course);
+  }, [courses.items, courseId]);
+
+  // If fetching or current course hasn't been set yet, show Loader
+  if (courses.isFetching || _.isEmpty(currentCourse)) {
+    return <Loader />;
+  }
+
+  return (
+    <MainContainer>
+      <HeaderContainer>
+        <h2>{currentCourse.name}</h2>
+      </HeaderContainer>
+    </MainContainer>
+  );
+};
 
 export default NewCoursePage;
