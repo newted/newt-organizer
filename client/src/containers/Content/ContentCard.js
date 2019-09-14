0568c8b37304b5207c0c861619058608819d753e
Nehal Udyavar <nehaludyavar@gmail.com>
2019-09-14 03:47:12 -0700

    Play around with card styling

--- client/src/containers/Content/ContentCard.js
+++ client/src/containers/Content/ContentCard.js
@@ -1,9 +1,13 @@
 import React from "react";
+import moment from "moment";
 // Styling
 import styles from "./ContentCard.module.css";
 
 const ContentCard = ({ content }) => (
-  <div className={styles.card}>{content.name}</div>
+  <div className={styles.card}>
+    <h4>{content.name}</h4>
+    <p>{`Due ${moment(content.dateDue).format("DD MMM")}`}</p>
+  </div>
 );
 
 export default ContentCard;
