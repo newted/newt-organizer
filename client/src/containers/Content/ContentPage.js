28ecb9fdd45cc2f60f257c6db265ed0a7a722440
Nehal Udyavar <nehaludyavar@gmail.com>
2019-09-14 17:29:14 -0700

    Initial Content Page layout

--- client/src/containers/Content/ContentPage.js
+++ client/src/containers/Content/ContentPage.js
@@ -4,10 +4,24 @@ import {
   MainContainer,
   ContentContainer
 } from "../../components/PageContainers";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+// Styling
+import styles from "./ContentPage.module.css";
 
-const ContentPage = () => (
+const ContentPage = ({ isFetching, content }) => (
   <MainContainer>
-    <ContentContainer>Content...</ContentContainer>
+    <ContentContainer className={styles.contentContainer}>
+      <Row>
+        <Col lg={3} md={12} className={styles.contentInfo}>
+          Content info...
+        </Col>
+        <Col lg="auto" md={0}></Col>
+        <Col lg md className={styles.content}>
+          Content...
+        </Col>
+      </Row>
+    </ContentContainer>
   </MainContainer>
 );
 
