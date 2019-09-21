402ba038490778a6da4e8be3138bbe19eca6f669
Nehal Udyavar <nehaludyavar@gmail.com>
2019-09-21 00:56:16 -0700

    LearnContentCard CSS + add course name to each content item

--- client/src/containers/Learn/LearnContentCard.js
+++ client/src/containers/Learn/LearnContentCard.js
@@ -2,11 +2,45 @@ import React from "react";
 import { Link } from "react-router-dom";
 // Styling
 import styles from "./LearnContentCard.module.css";
+import { FiFileText } from "react-icons/fi";
 
-const LearnContentCard = ({ userContent }) => (
-  <Link to={`/learn/${userContent._id}`}>
-    <div className={styles.card}>{userContent.name}</div>
-  </Link>
-);
+const LearnContentCard = ({ userContent, isActive }) => {
+  const setCardClass = (isActive, isComplete, cardVisual = false) => {
+    let cardStyle = [styles.card];
+    let cardVisualStyle = [styles.cardVisual];
+
+    if (isComplete) {
+      if (!cardVisual) {
+        cardStyle.push(styles.completedCard);
+      } else {
+        cardVisualStyle.push(styles.completedCardVisual);
+      }
+    }
+
+    if (isActive) {
+      if (!cardVisual) {
+        cardStyle.push(styles.activeCard);
+      } else {
+        cardVisualStyle.push(styles.activeCardVisual);
+      }
+    }
+
+    return cardVisual ? cardVisualStyle.join(" ") : cardStyle.join(" ");
+  };
+
+  return (
+    <Link to={`/learn/${userContent._id}`}>
+      <div className={setCardClass(isActive, userContent.isComplete)}>
+        <div className={setCardClass(isActive, userContent.isComplete, true)}>
+          <FiFileText size={30} color="#666" />
+        </div>
+        <div className={styles.cardBody}>
+          <h5>{userContent.name}</h5>
+          <div className={styles.subtitle}>{userContent.courseName}</div>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
 export default LearnContentCard;
--- client/src/containers/Learn/index.js
+++ client/src/containers/Learn/index.js
@@ -40,16 +40,13 @@ const LearnPage = ({ isFetching, userContents, match }) => {
 
   if (isFetching) return <Loader />;
 
-  // If the content data array is empty or the currentContent state hasn't been
-  // set yet, display message indicating so.
-  if (_.isEmpty(userContents) || !currentContent) {
-    return (
-      <MessageBox>
-        Looks like you haven't added anything to learn. Click on the Courses
-        tab, create a course, then add any content that you wish to learn to it.
-      </MessageBox>
-    );
-  }
+  // Message indicating to content exists and how to add some
+  const renderNoContent = () => (
+    <MessageBox>
+      Looks like you haven't added anything to learn. Click on the Courses tab,
+      create a course, then add any content that you wish to learn to it.
+    </MessageBox>
+  );
 
   return (
     <MainContainer>
@@ -57,14 +54,26 @@ const LearnPage = ({ isFetching, userContents, match }) => {
         <h2>Learn</h2>
       </HeaderContainer>
       <ContentContainer>
-        <div className={styles.contentList}>
-          {_.map(userContents, content => (
-            <LearnContentCard key={content._id} userContent={content} />
-          ))}
-        </div>
-        <div className={styles.contentFlow}>
-          <ContentFlow content={currentContent} />
-        </div>
+        {/* If the content data array is empty or the currentContent state
+            hasn't been set yet, render no content. */}
+        {_.isEmpty(userContents) || !currentContent ? (
+          renderNoContent()
+        ) : (
+          <>
+            <div className={styles.contentList}>
+              {_.map(userContents, content => (
+                <LearnContentCard
+                  key={content._id}
+                  userContent={content}
+                  isActive={content._id === currentContent._id}
+                />
+              ))}
+            </div>
+            <div className={styles.contentFlow}>
+              <ContentFlow content={currentContent} />
+            </div>
+          </>
+        )}
       </ContentContainer>
     </MainContainer>
   );
@@ -72,6 +81,11 @@ const LearnPage = ({ isFetching, userContents, match }) => {
 
 const mapStateToProps = ({ courses, userContent }) => {
   let userContentArray = _.values(userContent.items);
+
+  // Add course name for each user content
+  userContentArray.forEach(content => {
+    content.courseName = courses.items[content.courseId].name;
+  });
 
   // Sort by completion status and date
   statusDueDateSort(userContentArray);
