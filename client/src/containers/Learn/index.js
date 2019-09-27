c8707a54d3e6d7fc1a2003e0c8d9430b59c02870
Nehal Udyavar <nehaludyavar@gmail.com>
2019-09-27 16:00:28 -0700

    Take quiz functionality for Learn page

--- client/src/containers/Learn/index.js
+++ client/src/containers/Learn/index.js
@@ -13,11 +13,14 @@ import LearnContentCard from "./LearnContentCard";
 import ContentFlow from "../Content/ContentFlow";
 import EditContentModal from "../Content/EditContentModal";
 import DeleteItemModal from "../../components/Modal/DeleteItemModal";
+import QuizModal from "../../components/QuizModal";
 // API
 import {
   updateUserContent,
-  deleteUserContent
+  deleteUserContent,
+  addQuizToUserContent
 } from "../../actions/userContent";
+import { createPersonalQuiz, fetchQuiz } from "../../actions/quizzes";
 // Helpers
 import { statusDueDateSort } from "../../utils/containerHelpers";
 // Styling
@@ -26,14 +29,20 @@ import styles from "./Learn.module.css";
 const LearnPage = ({
   isFetching,
   userContents,
+  userQuizzes,
   updateUserContent,
   deleteUserContent,
+  createPersonalQuiz,
+  addQuizToUserContent,
+  fetchQuiz,
   match,
   history
 }) => {
   const [currentContent, setCurrentContent] = useState(null);
   const [showEditModal, setshowEditModal] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [showQuizModal, setShowQuizModal] = useState(false);
+  const [currentQuiz, setCurrentQuiz] = useState({});
   const { userContentId } = match.params;
 
   // Functions to set edit modal show state to true and false
@@ -42,6 +51,9 @@ const LearnPage = ({
   // Functions to set delete modal show state to true and false
   const handleShowDeleteModal = () => setShowDeleteModal(true);
   const handleCloseDeleteModal = () => setShowDeleteModal(false);
+  // Functions to set quiz modal to ...
+  const handleShowQuizModal = () => setShowQuizModal(true);
+  const handleCloseQuizModal = () => setShowQuizModal(false);
 
   // Use the userContentId from the url to get and set the currentContent from
   // the userContents array. If there's no id in the url, set it to the first
@@ -70,7 +82,38 @@ const LearnPage = ({
   // Function to handle deleting content
   const handleDeleteContent = () => {
     deleteUserContent(currentContent._id, currentContent.courseId);
+    history.push("/learn");
     handleCloseDeleteModal();
+  };
+
+  // Handler function for when the 'Take the quiz' button is clicked
+  const handleTakeQuiz = userContent => {
+    // Open quiz modal
+    handleShowQuizModal();
+
+    if (_.isEmpty(userContent.quizInfo)) {
+      // Create personal quiz and update assignment
+      const data = {
+        contentId: userContent.contentInfo.contentId,
+        userContentId: userContent._id
+      };
+      // Dispatch action to create quiz, then update user content to add quiz id
+      createPersonalQuiz(data).then(quiz => {
+        addQuizToUserContent(userContent._id, {
+          quizId: quiz._id
+        }).then(() => setCurrentQuiz(quiz));
+      });
+    } else {
+      // If quiz has already been fetched, set to state, otherwise fetch quiz first
+      if (userQuizzes[userContent.quizInfo[0].quizId]) {
+        setCurrentQuiz(userQuizzes[userContent.quizInfo[0].quizId]);
+      } else {
+        // Fetch personal quiz using id
+        fetchQuiz(userContent.quizInfo[0].quizId).then(quiz =>
+          setCurrentQuiz(quiz)
+        );
+      }
+    }
   };
 
   if (isFetching) return <Loader />;
@@ -109,6 +152,7 @@ const LearnPage = ({
                 content={currentContent}
                 showEditModal={handleShowEditModal}
                 showDeleteModal={handleShowDeleteModal}
+                onTakeQuiz={handleTakeQuiz}
               />
             </div>
           </>
@@ -127,13 +171,21 @@ const LearnPage = ({
             onHide={handleCloseDeleteModal}
             onDelete={handleDeleteContent}
           />
+          <QuizModal
+            show={showQuizModal}
+            handleCloseModal={handleCloseQuizModal}
+            quizName={`Quiz for ${currentContent.name}`}
+            quiz={currentQuiz}
+            // showReview={isQuizComplete()}
+            // onComplete={handleCompleteQuiz}
+          />
         </>
       )}
     </MainContainer>
   );
 };
 
-const mapStateToProps = ({ courses, userContent }) => {
+const mapStateToProps = ({ courses, userContent, quizzes }) => {
   let userContentArray = _.values(userContent.items);
 
   // Add course name for each user content
@@ -146,11 +198,18 @@ const mapStateToProps = ({ courses, userContent }) => {
 
   return {
     isFetching: courses.isFetching || userContent.isFetching,
-    userContents: userContentArray
+    userContents: userContentArray,
+    userQuizzes: quizzes.items
   };
 };
 
-const mapDispatchToProps = { updateUserContent, deleteUserContent };
+const mapDispatchToProps = {
+  updateUserContent,
+  deleteUserContent,
+  createPersonalQuiz,
+  fetchQuiz,
+  addQuizToUserContent
+};
 
 export default connect(
   mapStateToProps,
